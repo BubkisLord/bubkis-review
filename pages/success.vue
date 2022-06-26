b42954b1dceed84515ecdf828ee9dd8a68e9b1e6
BubkisLord <101391373+BubkisLord@users.noreply.github.com>
2022-06-26 12:43:10 +0800

    Update success.vue

--- pages/success.vue
+++ pages/success.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <h1>Thanks for your feedback!</h1>
-    <NuxtLink to="/">Home</NuxtLink>
+    <NuxtLink to="/">Go back to review page</NuxtLink>
+    <NuxtLink to="https://bubkis.me/">Go to main website</NuxtLink>
   </div>
 </template>
