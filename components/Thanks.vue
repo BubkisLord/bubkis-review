bd50c82b59ef071b1c76dc22fb30fd7777c33f4f
BubkisLord <101391373+BubkisLord@users.noreply.github.com>
2022-06-26 17:45:37 +0800

    Update Thanks.vue

--- components/Thanks.vue
+++ components/Thanks.vue
@@ -1,1 +1,14 @@
+<script>
+export default {
+  data: () => ({
+    joke: '',
+  }),
+  async mounted() {
+    this.joke = await fetch('/api/thanksmessage').then((res) => res.json())
+  },
+}
+</script>
 
+<template>
+  <p>{{ thanksmessage }}</p>
+</template>
