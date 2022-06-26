aca9834ed7800517eb3fe7bd1e0fc2d195a12fbc
BubkisLord <101391373+BubkisLord@users.noreply.github.com>
2022-06-26 12:47:42 +0800

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,5 @@
 <template>
+  <script src="https://www.google.com/recaptcha/api.js"></script>
   <main>
     <div class="intro">
       <h1>Bubkis Games Review</h1>
@@ -6,6 +7,10 @@
         Hey! Do you want to give Bubkis Games a review? How did you like our games? What do you want to see in the future? What could be improved? Do you want me to do a specific project? Do you want me to mod a game? Do you want me to <strong>make</strong> a game? Tell me here!
       </p>
     </div>
+    <button class="g-recaptcha" 
+        data-sitekey="reCAPTCHA_site_key" 
+        data-callback='onSubmit' 
+        data-action='submit'>Submit</button>
     <FeedbackForm />
     <JokeBlock />
   </main>
@@ -21,3 +26,8 @@
   width: 60%;
 }
 </style>
+ <script>
+   function onSubmit(token) {
+     document.getElementById("demo-form").submit();
+   }
+ </script>
